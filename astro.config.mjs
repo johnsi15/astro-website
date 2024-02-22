ffe806180782e92c2cae30f29472d9eef459d711
John Serrano <andrey_201015@hotmail.com>
2024-02-22 09:36:27 -0500

    Add rss content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact()]
-});
+  integrations: [tailwind(), preact()],
+  site: 'https://extraordinary-horse-d6cbd9.netlify.app',
+})
